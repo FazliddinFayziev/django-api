9f1763e19f8c0cd2cf96b80a249e5c2aed19d43d
FazliddinFayziev <fazrez4515@gmail.com>
2023-11-17 10:43:15 +0800

    working with migrate

--- store/models.py
+++ store/models.py
@@ -19,8 +19,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
@@ -80,6 +81,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255, null=True)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
 
 
